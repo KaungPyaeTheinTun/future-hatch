e37c76f365b4e3d362a42d36bf97771ce223eedb
Kaung Pyae Thein Tun <kaungpyaetheintun@Kaungs-MacBook-Air.local>
2026-05-23 10:35:38 +0630

    nitro

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import { nitro } from "nitro/vite";
+import { nitro } from "nitro/vite"; // <-- 1. Make sure this is imported
 
 export default defineConfig({
   plugins: [
-    tanstackStart({
-      prerender: {
-        enabled: false, // 👈 Fixes the Vercel crash by bypassing local build pipeline checks
-      },
-    }),
-    nitro({
-      preset: "vercel",
-    }),
+    tanstackStart(),
+    nitro(), // <-- 2. Make sure this is called here
     viteReact(),
   ],
 });
